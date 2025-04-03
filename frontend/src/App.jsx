8463ede33f7d47e2bf619dbf0913d08d9ebda8e8
Ravin Yadav <yravin72@gmail.com>
2025-04-03 12:35:14 +0530

    fetching and deleting the product and rendering on th e frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes,Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import ProductPage from "./pages/ProductPage"
 import { useThemeStore } from "./store/useThemeStore";
+import {Toaster} from "react-hot-toast";
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<HomePage/>} />
             <Route path="/products/:id" element={<ProductPage/>} />
           </Routes>
+          <Toaster/>
       </div>
   )
 }
---
+++ frontend/src/store/useProductStore.js
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const BASE_URL = "http://localhost:3000"
+export const useProductStore = create((set, get) => ({
+    // products state
+    products: [],
+    loading: false,
+    error: null,
+
+    fetchProducts: async () => {
+        set({ loading: true });
+        try {
+            const response = await axios.get(`${BASE_URL}/api/products`)
+            set({ products: response.data?.data, error: null })
+        } catch (err) {
+            if (err.status == 429) {
+                set({ error: "Rate Limit Exceed", products: [] })
+            } else {
+                set({ error: "Something went wrong", products: [] })
+            }
+        }
+        finally {
+            set({ loading: false });
+        }
+    },
+
+    deleteProduct: async (id) => {
+        console.log("deleteProduct function called", id);
+        set({ loading: true });
+        try {
+          await axios.delete(`${BASE_URL}/api/products/${id}`);
+          set((prev) => ({ products: prev.products.filter((product) => product.id !== id) }));
+          toast.success("Product deleted successfully");
+        } catch (error) {
+          console.log("Error in deleteProduct function", error);
+          toast.error("Something went wrong");
+        } finally {
+          set({ loading: false });
+        }
+      },
+}))
