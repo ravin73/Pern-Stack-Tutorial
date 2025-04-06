1042eaf1b740ac60882e11c5eca53a2c323a1364
Ravin Yadav <yravin72@gmail.com>
2025-04-06 15:00:20 +0530

    make production ready application

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = "http://localhost:3000"
+// base url will be dynamic depending on the environment
+const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:3000":""
+
 export const useProductStore = create((set, get) => ({
   // products state
   products: [],
